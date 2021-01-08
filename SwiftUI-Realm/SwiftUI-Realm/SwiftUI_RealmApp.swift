b34ad4ef8d6106b6394b0a749f4036475de887cd
Macostik <y.granchenko@gmail.com>
2021-01-08 11:23:13 +0200

    create preject

---
+++ SwiftUI-Realm/SwiftUI-Realm/SwiftUI_RealmApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_RealmApp.swift
+//  SwiftUI-Realm
+//
+//  Created by Maxim Granchenko on 08.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_RealmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
